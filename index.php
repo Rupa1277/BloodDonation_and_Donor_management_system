4844bede9179115e4163a276feb2f2b4127f2e84
Rupali Arlekar <rupa1277@gmail.com>
2026-03-15 20:23:27 +0530

    Initial Blood Bank Project Structure

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include("config/database.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Blood Bank System</title>
+</head>
+
+<body>
+
+<h1>Blood Bank & Donor Management System</h1>
+
+<p>Website is working!</p>
+
+</body>
+</html>
